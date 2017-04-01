5b7244ed0c4218120c88919f2fa69b3a0476d14d
Mike Izbicki <mike@izbicki.me>
2017-04-01 02:52:43 -0700

    linear changes

--- src/mnist/models/linear.py
+++ src/mnist/models/linear.py
@@ -4,6 +4,11 @@ import tensorflow as tf
 from common import *
 
 def inference(images,_):
+    #x_images=tf.reshape(images,[100,28,28])
+    #W = tf.Variable(tf.zeros([28,28,10]))
+    #b = tf.Variable(tf.zeros([10]))
+    #y = tf.tensordot(x_images,W,[[1,2],[0,1]]) + b
+
     W = tf.Variable(tf.zeros([784,10]))
     b = tf.Variable(tf.zeros([10]))
     y = tf.matmul(images,W) + b
